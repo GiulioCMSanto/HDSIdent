2ecb322000de81f3f76db4c7363adefd99d6ca04
GiulioCMSanto <giuliosanto@gmail.com>
2020-03-06 16:45:56 -0300

    change_point/exponential_weighted.py: removed comments
    - Removed comments and included longer grid

--- src/change_point/exponential_weighted.py
+++ src/change_point/exponential_weighted.py
@@ -460,12 +460,10 @@ class ExponentialWeighted(object):
         original_X = self.X
         
         #Grid of forgetting factors
-        #factor_grid = [0.0001, 0.0005, 0.001, 0.002, 0.003, 0.004, 0.005, 0.01]
-        factor_grid = [0.003, 0.004, 0.005, 0.01]
+        factor_grid = [0.0001, 0.0005, 0.001, 0.002, 0.003, 0.004, 0.005, 0.01]
         
         #Grid of Thresholds
-        #H_v_grid = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 0.7, 1]
-        H_v_grid = [0.2, 0.5, 0.7, 1]
+        H_v_grid = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 0.7, 1]
         
         #Iterate Over Grid
         if individual_plot:
