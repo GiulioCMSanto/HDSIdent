bc848291a904a1c980b9164754be95666ed8a3a1
GiulioCMSanto <giuliosanto@gmail.com>
2020-05-12 19:37:00 -0300

    exponential_weighted.py and laguerre_filter.py: corrections
    exponential_weighted.py: changed the parameters range in the
    surface plot
    
    laguerre_filter.py: corrected error on segments loop. The segments
    keys were not being used, but the range() function was being used
    instead.

--- HDSIdent/initial_intervals/exponential_weighted.py
+++ HDSIdent/initial_intervals/exponential_weighted.py
@@ -464,7 +464,7 @@ class ExponentialWeighted(object):
         factor_grid = [0.0001, 0.0005, 0.001, 0.002, 0.003, 0.004, 0.005, 0.01]
         
         #Grid of Thresholds
-        H_v_grid = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 0.7, 1]
+        H_v_grid = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.008, 0.1]
         
         #Iterate Over Grid
         if individual_plot:
